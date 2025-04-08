8bae6396f5dda6a1403e695e71c0630d0d828daa
guzmonne <guzmonne@hotmail.com>
2025-04-08 13:54:36 -0300

    feat(smtp): Implement basic SMTP server functionality
    fix(smtp): Correct log level environment variable name

--- crates/smtp/src/cli.rs
+++ crates/smtp/src/cli.rs
@@ -4,12 +4,16 @@ use clap::Parser;
 #[command(name = "quillai-smtp")]
 #[command(about = "Quillai SMTP")]
 pub struct App {
+    /// Socket to bind the STMP server.
+    #[clap(long, default_value = "127.0.0.1:2525", env = "QUILLAI_SMTP_BIND")]
+    pub bind: std::net::SocketAddr,
+
     /// Log level
     #[clap(
         long,
         value_enum,
         default_value = "warn",
-        env = "QUILLAI_API_LOG_LEVEL"
+        env = "QUILLAI_SMTP_LOG_LEVEL"
     )]
     pub log_level: quillai_log::LogLevel,
 }
--- crates/smtp/src/main.rs
+++ crates/smtp/src/main.rs
@@ -2,6 +2,10 @@ mod cli;
 mod prelude;
 
 use clap::Parser;
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, BufWriter};
+
+#[allow(unused_imports)]
+use tokio::net::{TcpListener, TcpStream};
 
 use crate::prelude::*;
 
@@ -19,5 +23,82 @@ async fn main() -> Result<()> {
         .filter_level(log::LevelFilter::from(args.log_level))
         .init();
 
+    // ╭─────────────────────────────────────────────────────────────────────────────╮
+    // │ Start listening                                                             │
+    // ╰─────────────────────────────────────────────────────────────────────────────╯
+    listen(args.bind).await
+}
+
+#[allow(dead_code)]
+async fn listen(addr: std::net::SocketAddr) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+    log::trace!("SMTP server listening on {}", addr);
+
+    loop {
+        match listener.accept().await {
+            #[allow(unused_variables)]
+            #[allow(unused_mut)]
+            Ok((mut stream, _)) => {
+                tokio::spawn(async move {
+                    if let Err(err) = handle_connection(&mut stream).await {
+                        log::trace!("Error handling SMTP connection: {:?}", err);
+                    };
+                });
+            }
+            Err(err) => {
+                log::error!("Error establishing SMTP connection: {:?}", err);
+            }
+        }
+    }
+}
+
+async fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+    let (reader, writer) = stream.split();
+    let mut reader = BufReader::new(reader);
+    let mut writer = BufWriter::new(writer);
+
+    writer
+        .write_all(format!("220 {}\r\n", "My SMTP Server").as_bytes())
+        .await?;
+    writer.flush().await?;
+
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+        let bytes_read = reader.read_line(&mut line).await?;
+        log::trace!("Read {} bytes: {:?}", bytes_read, line);
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        writer
+            .write_all(format!("Read {} bytes", bytes_read).as_bytes())
+            .await?;
+        writer.flush().await?;
+    }
+
     Ok(())
+
+    // loop {
+    //     line.clear();
+    //     let bytes_read = reader.lines
+    //         (&mut line).await?;
+    //     if bytes_read == 0 {
+    //         break;
+    //     }
+    //
+    //     let response = match line.trim() {
+    //         "HELO" => "250 Hello\r\n",
+    //         "QUIT" => {
+    //             writer.write_all(b"221 Bye\r\n").await?;
+    //             break;
+    //         }
+    //         _ => "500 Unrecognized command\r\n",
+    //     };
+    //
+    //     writer.write_all(response.as_bytes()).await?;
+    //     writer.flush().await?;
+    // }
 }
