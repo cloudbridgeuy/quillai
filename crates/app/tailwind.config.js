e7efa197ed1efc9b1f4eb04ae8160ec3470038ca
guzmonne <guzmonne@hotmail.com>
2025-06-23 22:35:49 -0300

    chore: remove app

--- crates/app/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  mode: "all",
-  content: ["./src/**/*.{js,rs,html,css}", "./dist/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
